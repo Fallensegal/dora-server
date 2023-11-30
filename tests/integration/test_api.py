61097e9028238a6e96d01ff6118df9b644113f30
Wasif Islam <w.islam@obscuritylabs.com>
2023-11-29 22:12:35 -0500

    yolo

--- tests/integration/test_api.py
+++ tests/integration/test_api.py
@@ -11,15 +11,19 @@ def test_client() -> Generator[TestClient, None, None]:
     with TestClient(app) as client:
         yield client
 
+
 def test_user_submit_dora_metrics(test_client: TestClient) -> None:
-    res = test_client.post("/metrics", json={
-        "user_id": "b0098d7a-b10e-44af-b61d-3e87c69c197c",
-        "metric_1": 1,
-        "metric_2": 2,
-        "metric_3": 3,
-        "metric_4": 4,
-        "metric_5": 5,
-    })
+    res = test_client.post(
+        "/metrics",
+        json={
+            "user_id": "b0098d7a-b10e-44af-b61d-3e87c69c197c",
+            "metric_1": 1,
+            "metric_2": 2,
+            "metric_3": 3,
+            "metric_4": 4,
+            "metric_5": 5,
+        },
+    )
     assert res.status_code == status.HTTP_201_CREATED
 
 
