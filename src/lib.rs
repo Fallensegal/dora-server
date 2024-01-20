41a86d6fdb01eb761f8e1480407b5d61493aca9a
Wasif Islam <islamwasif3@gmail.com>
2024-01-19 22:33:53 -0500

    move tests to translation unit

--- src/lib.rs
+++
@@ -1,9 +0,0 @@
-
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn hello_test() {
-        let hello_world: &str = "Hello World";
-        assert_eq!(hello_world, "Hello World");
-    }
-}
--- src/main.rs
+++ src/main.rs
@@ -27,3 +27,14 @@ async fn main() -> std::io::Result<()> {
     .run()
     .await
 }
+
+/* Unit Tests */
+
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn hello_test() {
+        let hello_world: &str = "Hello World";
+        assert_eq!(hello_world, "Hello World");
+    }
+}
